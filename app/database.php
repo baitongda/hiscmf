eb5e6d158671baeb32fc2a69bd1a91eb3d215f57
liyuzhao <562405704@qq.com>
2017-05-23 16:04:35 +0800

    项目安装的方法

--- app/database.php
+++ app/database.php
@@ -13,8 +13,7 @@ return [
     // 数据库类型
     'type'           => 'mysql',
     // 服务器地址
-    //'hostname'       => '120.27.108.53',
-    //'hostname'       => 'localhost',
+    'hostname'       => 'localhost',
     // 数据库名
     'database'       => 'his',
     // 用户名
